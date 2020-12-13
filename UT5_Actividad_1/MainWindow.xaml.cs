03a074702cf2d6c792d4ef2b4195c581d9d709cc
Daniel Alonso <de.alonso@iesdoctorbalmis.com>
2020-12-13 21:32:41 +0100

    Agregar archivos de proyecto.

---
+++ UT5_Actividad_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UT5_Actividad_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Plato> platos;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            nombresComboBox();
+            platos = Plato.GetSamples("C:/Users/dealo/OneDrive/Pictures/Imagenes");
+            PlatosListBox.DataContext = platos;         
+        }
+        
+        private void nombresComboBox()
+        {
+            List<String> paises = new List<string>() { "Americana", "China", "Mexicana"};
+            opcionesComboBox.ItemsSource = paises;
+        }
+
+        private void PlatosListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            formularioStackPanel.DataContext = platos[PlatosListBox.SelectedIndex];
+        }
+    }
+}
---
+++ UT5_Actividad_1/paisTipoConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UT5_Actividad_1
+{
+    class paisTipoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string pathImage = "";
+            switch (value)
+            {
+                case "Americana":
+                    pathImage = "united_states.png";
+                    break;
+                case "China":
+                    pathImage = "china.png";
+                    break;
+                case "Mexicana":
+                    pathImage = "mexico.png";
+                    break;
+            }
+            return pathImage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
